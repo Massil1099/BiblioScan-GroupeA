2c8f39c9e2c3421b9616b5e97c2b8ea6a041f9ab
MASSIL <massilaitchallal2002@gmail.com>
2025-05-09 15:37:03 +0200

    Resolution conflits et architecture de base pour l'appli backend/frontend séparés

--- ktor-sample/build.gradle.kts
+++ ktor-sample/build.gradle.kts
@@ -1,12 +1,11 @@
 
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.ktor)
-
-
-    kotlin("plugin.serialization") version "2.1.20"
-
+    kotlin("jvm") 
+    kotlin("plugin.serialization") 
+    id("io.ktor.plugin") version "2.3.4"
+    application
 }
+
 
 group = "com.example"
 version = "0.0.1"
@@ -14,7 +13,7 @@ version = "0.0.1"
 application {
     mainClass = "io.ktor.server.netty.EngineMain"
 
-    val isDevelopment: Boolean = project.ext.has("development")
+    val isDevelopment: Boolean = project.hasProperty("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
@@ -23,20 +22,17 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.config.yaml)
-    testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test.junit)
+    implementation("io.ktor:ktor-server-core:2.3.4")
+    implementation("io.ktor:ktor-server-netty:2.3.4")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("io.ktor:ktor-server-config-yaml:2.3.4")
 
-
-
+    testImplementation("io.ktor:ktor-server-tests:2.3.4")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.21")
 
     implementation("io.ktor:ktor-server-content-negotiation:2.3.4")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     implementation("io.ktor:ktor-server-default-headers:2.1.20")
-
 }
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "BiblioScan"
 include(":app")
- 
+include(":ktor-sample")
